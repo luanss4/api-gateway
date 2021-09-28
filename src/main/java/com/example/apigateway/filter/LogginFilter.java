ad3ddab68536db765e47d021156dfb420366ea8b
Luan Sousa <luanss4@gmail.com>
2021-09-28 17:37:30 -0300

    adicao de filter de log para rotas

--- src/main/java/com/example/apigateway/filter/LogginFilter.java
+++ src/main/java/com/example/apigateway/filter/LogginFilter.java
@@ -14,7 +14,7 @@ public class LogginFilter implements GlobalFilter {
     private Logger logger = LoggerFactory.getLogger(LogginFilter.class);
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
-        logger.info("Original request path: {} ", exchange.getRequest().getPath());
+//        logger.info("Original request path: {} ", exchange.getRequest().getPath());
         return chain.filter(exchange);
     }
 }
